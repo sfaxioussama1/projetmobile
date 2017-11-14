63ff425eb22b522849fe9c86cc3010461cf3ace3
Lindagasdalah <32685142+Lindagasdalah@users.noreply.github.com>
2017-11-14 23:06:10 +0100

    interface

--- app/src/main/java/tn/agil/miniprojet/agil/AfficherProduit.java
+++ app/src/main/java/tn/agil/miniprojet/agil/AfficherProduit.java
@@ -12,13 +12,7 @@ public class AfficherProduit extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String[] values = new String[]{"TANIX 4WD SAE 15W50",
-                "TANIX DIESEL 700 SAE 10W40", "TANIX DIESEL 500 SAE 40",
-                "ENI I-SINT 10W40"};
 
-        MonAdaptateurDeListe adaptateur = new MonAdaptateurDeListe(this, values);
-
-        setListAdapter(adaptateur);
     }
     protected void onListItemClick(ListView l, View v, int position, long id) {
         Toast.makeText(this, "Position : " + position, Toast.LENGTH_LONG).show();
