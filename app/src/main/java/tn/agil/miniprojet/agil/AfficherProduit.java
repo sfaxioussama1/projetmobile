70bfe2d39a3c99c08bb26c452206bfc04c883b3e
Lindagasdalah <32685142+Lindagasdalah@users.noreply.github.com>
2017-11-16 23:22:33 +0100

    final

--- app/src/main/java/tn/agil/miniprojet/agil/AfficherProduit.java
+++ app/src/main/java/tn/agil/miniprojet/agil/AfficherProduit.java
@@ -1,4 +1,8 @@
 package tn.agil.miniprojet.agil;
+/**
+ * Created by Linda on 12/11/2017.
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,127 +13,131 @@ import java.util.List;
 import java.util.Map;
 
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 public class AfficherProduit extends Activity {
-    private String jsonResult;
-    String recupProduitUrl="http://192.168.1.5/WebService/listeproduit.php";
-    //private String url ="http://10.0.2.2/WebService/listeproduit.php";
-    private ListView listView;
+    String showUrl="http://192.168.1.5:81/WebService1/listepromoprod.php";
+    private ListView listView=null;
+    private ArrayList<String> list = new ArrayList<String>();
+    RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afficher_produit);
         listView = (ListView) findViewById(R.id.listView1);
-        accessWebService();
+        requestQueue = Volley.newRequestQueue(this);
+        recup();
     }
 
-    /*  @Override
-      public boolean onCreateOptionsMenu(Menu menu) {
-          // Inflate the menu; this adds items to the action bar if it is present.
-          getMenuInflater().inflate(R.menu.main, menu);
-          return true;
-      }
-  */
-    // Async Task to access the web
-    private class JsonReadTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(params[0]);
-            try {
-                HttpResponse response = httpclient.execute(httppost);
-                jsonResult = inputStreamToString(
-                        response.getEntity().getContent()).toString();
-            }
+    public void recup(){
 
-            catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
 
-        private StringBuilder inputStreamToString(InputStream is) {
-            String rLine = "";
-            StringBuilder answer = new StringBuilder();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
 
-            try {
-                while ((rLine = rd.readLine()) != null) {
-                    answer.append(rLine);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,showUrl,new Response.Listener<JSONObject>(){
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                    JSONArray produits = response.getJSONArray("produits");
+                    int i=0;
+
+                    for (i = 0; i < produits.length(); i++) {
+                        JSONObject produit = produits.getJSONObject(i);
+                        //add list
+
+                        String nom = produit.getString("nomproduit");
+                        String quantite = produit.getString("quantite");
+                        String prix= produit.getString("prixfinal");
+                        String dateD= produit.getString("datedebut");
+                        String dateF= produit.getString("datefin");
+
+                        // String marque = produit
+                        String ligne = "- Produit : "+nom+"\n Qantité : "+quantite+"  \n prix : "+prix
+                                +"\n Date Debut : "+dateD+" \n  Date Fin : "+dateF;
+                        list.add(ligne);
+
+                    }
+
+                    String[] items = new String[list.size()];
+                    items = list.toArray(items);
+
+                    ArrayAdapter<String> adapter=new ArrayAdapter<String>(AfficherProduit.this,
+                            R.layout.list_item, R.id.txtitem,items);
+
+                    listView.setAdapter(adapter);
+
+                    // Perform action when an item is clicked
+
+                    listView.setOnItemClickListener(new
+                                                            AdapterView.OnItemClickListener() {
+
+                                                                @Override
+
+                                                                public void onItemClick(AdapterView<?> parent, View view, int
+                                                                        position, long id) {
+
+                                                                    ViewGroup vg=(ViewGroup)view;
+                                                                    String item = listView.getItemAtPosition(position).toString();
+
+
+
+                                                                }
+
+                                                            });
+                    // Toast.makeText(MainActivity.this, list.toString(), Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    Toast.makeText(AfficherProduit.this, "Ereur DB", Toast.LENGTH_SHORT).show();
+
                 }
+
             }
+        },
 
-            catch (IOException e) {
-                // e.printStackTrace();
-                Toast.makeText(getApplicationContext(),
-                        "Error..." + e.toString(), Toast.LENGTH_LONG).show();
-            }
-            return answer;
-        }
+                new Response.ErrorListener(){
 
-        @Override
-        protected void onPostExecute(String result) {
-            ListDrwaer();
-        }
-    }// end async task
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(AfficherProduit.this, "Erreur de connexion", Toast.LENGTH_SHORT).show();
 
-    public void accessWebService() {
-        JsonReadTask task = new JsonReadTask();
-        // passes values for the urls string array
-        task.execute(new String[] { recupProduitUrl });
+                    }
+
+
+
+                }){
+
+
+        };
+        requestQueue.add(jsonObjectRequest);
+
     }
 
-    // build hash set for list view
-    public void ListDrwaer() {
-        List<Map<String, String>> produitList = new ArrayList<Map<String, String>>();
-
-        try {
-            JSONObject jsonResponse = new JSONObject(jsonResult);
-            JSONArray jsonMainNode = jsonResponse.optJSONArray("produits");
-
-            for (int i = 0; i < jsonMainNode.length(); i++) {
-                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
-                String name = jsonChildNode.optString("nomproduit");
-                String number = jsonChildNode.optString("quantite");
-
-
-                String outPut = name + "-" + number;
-                produitList.add(createProduit("Produits", outPut));
-            }
-        } catch (JSONException e) {
-            Toast.makeText(getApplicationContext(), "Error" + e.toString(),
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, produitList,
-                android.R.layout.simple_list_item_1,
-                new String[] { "Produits" }, new int[] { android.R.id.text1 });
-        listView.setAdapter(simpleAdapter);
-    }
-
-    private HashMap<String, String> createProduit(String name , String number) {
-        HashMap<String, String> produitsNameNo = new HashMap<String, String>();
-        produitsNameNo.put(name, number);
-        return produitsNameNo;
-    }
 }
-
